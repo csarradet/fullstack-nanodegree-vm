7500f9b844c11e8ac1ccf619cfd50c0a44fd24ea
Cody Sarradet <csarradet@gmail.com>
2016-03-17 13:53:02 -0500

    chore: Cleanup pass, removed templates used as scaffolding during DAL/endpoint implementation

--- vagrant/catalog/catalog.sql
+++ vagrant/catalog/catalog.sql
@@ -34,7 +34,7 @@ CREATE TABLE items (
     item_id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT NOT NULL,
-    pic BLOB NOT NULL,
+    pic BLOB NOT NULL, #Binary JPEG data, base64 encoded
     cat_id INTEGER NOT NULL,
     creator_id INTEGER NOT NULL,
     changed DATETIME NOT NULL,
